f4fa84328d271094abe5460488b9886d72480929
hendrickn <45163672+hendrickn@users.noreply.github.com>
2018-11-19 11:13:35 +0100

    Update fontion_jeu.py

--- fontion_jeu.py
+++ fontion_jeu.py
@@ -1,6 +1,12 @@
+from time import*
+
 def test_fin_jeu (grille) :
     res=grille.T
     for i in res :
         if 0 in i :
             return False
     return True
+
+
+def horloge(n,difficulté) :
+    sleep(max(0.2,1-difficulté/(difficulté+1+n)))
