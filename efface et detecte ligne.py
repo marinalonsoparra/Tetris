05824c4c6077723d10673d8091dc3f6c86c49912
SamuelHulin <45163798+SamuelHulin@users.noreply.github.com>
2018-11-19 11:28:30 +0100

    Update efface et detecte ligne.py

--- efface et detecte ligne.py
+++ efface et detecte ligne.py
@@ -1,22 +1,31 @@
 import numpy as np
 
 
-def effacer_ligne(grille, i):
-    grille_liste = grille.tolist()
-    grille_liste = [grille_liste.pop(i)] + grille_liste
-    return np.array(grille_liste)
+# efface la ligne i de la grille et crée une nouvelle ligne en haut de la grille
+def effacer_ligne(grille, i): 
+    a = grille.pop(i)
+    grille = [[0 for i in range(grille(0))]] + grille
+    return grille
 
 
+# regarde si la ligne i de la grille est pleine
 def ligne_pleine(grille, i):
-    grille_liste = grille.tolist()
-    for k in range(len(grille_liste[i])):
+    for k in range(len(grille[i])):
         if grille_liste[i][k] == 0:
             return False
     return True
 
+
+# renvoie la liste des lignes pleines de la grille
 def detecte_ligne(grille):
     liste_lignes_pleines = []
     for i in range(len(grille)):
         if ligne_pleine(grille, i):
             liste_lignes_pleines.append(i)
     return liste_lignes_pleines
+
+
+# renvoie la grille après avoir supprimé les lignes pleines
+def traitement_grille(grille):
+    for c in detecte_ligne(grille):
+        grille = effacer_ligne(grille, c)
