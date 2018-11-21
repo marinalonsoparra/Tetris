ae0b0edc245a8a02a49c4071e2d2ca90fb1ff823
2018hendrickn <niels.hendrickx@student-cs.fr>
2018-11-21 10:56:23 +0100

    début Thread

--- fontion_jeu.py
+++ fontion_jeu.py
@@ -3,28 +3,28 @@
 from deplacement_tetris.py import *
 from time import*
 import numpy as np
-
+from deplacement_tetris import*
 def test_fin_jeu (grille) :
-res=np.array(grille).T
-for i in res :
-if len(i)>22 :
-return True
-return False
+    res=np.array(grille).T
+    for i in res :
+        if len(i)>22 :
+            return True
+    return False
 
 
 def horloge(n, difficulte) :
-sleep(max(0.2, 1 - difficulte / (difficulte + 1 + n)))
+    sleep(max(0.2, 1 - difficulte / (difficulte + 1 + n)))
 
 def collision(piece,grille) :
-Liste_piece=coordonees(piece)
-grille_copy=copy.deepcopy(grille)
-for i in Liste_piece :
-if i[0]==22 :
-for j in liste_piece :
-    grille[j[0]][j[1]]+=pieces_etat.keys()[pieces_etat.values().index(piece)]
-else :
-if grille[i[0]][i[1]+1]!=0 :
-    for k in liste_piece :
-        grille_copy[k[0]][k[1]]+=pieces_etat.keys()[pieces_etat.values().index(piece)]
-    return (True,grille_copy)
-return (False,grille)
+    Liste_piece=coordonees(piece)
+    grille_copy=copy.deepcopy(grille)
+    for i in Liste_piece :
+        if i[0]==22 :
+            for j in Liste_piece :
+                grille[j[0]][j[1]]+=pieces_etat.keys()[pieces_etat.values().index(piece)]
+        else :
+            if grille[i[0]][i[1]+1]!=0 :
+                for k in Liste_piece :
+                    grille_copy[k[0]][k[1]]+=pieces_etat.keys()[pieces_etat.values().index(piece)]
+                return (True,grille_copy)
+    return (False,grille)
