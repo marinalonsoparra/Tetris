a5a6cff12da02a1ba54b8dfa111f00a806a34ff5
timomaszewski <timo.maszewski@student-cs.fr>
2018-11-21 09:32:46 +0100

    Merge branch 'master' of https://github.com/marinalonsoparra/Tetrix
    # Conflicts:
    #	deplacement_tetris.py

--- affichage.py
+++ affichage.py
@@ -1,9 +1,19 @@
 from Tkinter import *
+from deplacement_tetris import *
+from grille_de_jeu import *
+from pieces_etats import *
+
+root = Tk()
+root.title("Tetris")
+top = Toplevel()
+
+grille = cree_grille()
+grille_graphique = [[[0] for _ in range(10)] for _ in range(22)]
+for i in range(22):
+    for j in range(10):
+        case = Frame(top, row = i, column = j)
+        grille_graphique[j][i] = case
 
 
-window = Tk()
-window.title("Tetris")
-
-
-
-window.mainloop()
+root.mainloop()
+top.mainloop()
