c1242620623c15862b7f1ad94ba2cafd40185339
2018hendrickn <niels.hendrickx@student-cs.fr>
2018-11-21 14:42:25 +0100

    Modif déplacement bas affichage

--- affichage.py
+++ affichage.py
@@ -5,6 +5,13 @@ from fontion_jeu import *
 import time
 import pygame
 
+def sup(timer) :
+    if time.clock()-timer >=1 :
+        timer=time.clock()
+        return True
+    else :
+        return False
+
 
 def affichage_grille():
 
@@ -57,8 +64,10 @@ def affichage_grille():
         global piece
         global niveau
         mise_a_jour_grille_graph()
+        timer=time.clock()
         while not test_fin_jeu():
-            horloge(niveau)
+            if sup(timer) :
+                piece=deplacement_piece(grille,piece,'b')
             if collision(piece, grille)[0]:
                 grille = collision(grille)[1]
                 piece = generer_piece()
