e9021ed3f1580fa0462a4c232452722c14307efe
timomaszewski <timo.maszewski@student-cs.fr>
2018-11-21 10:53:08 +0100

    Merge branch 'master' of https://github.com/marinalonsoparra/Tetrix
    # Conflicts:
    #	deplacement_tetris.py

--- affichage.py
+++ affichage.py
@@ -3,49 +3,51 @@ from deplacement_tetris import *
 from grille_de_jeu import *
 from pieces_etats import *
 
-root = Tk()
-root.title("Tetris")
-top = Toplevel()
+def affichage_grille():
+    root = Tk()
+    root.title("Tetris")
+    top = Toplevel()
 
-global grille
-grille = cree_grille()
-global grille_graphique
-grille_graphique = [[0 for _ in range(10)] for _ in range(22)]
-
-global piece
-piece = generer_piece()
-
-for i in range(22):
-    for j in range(10):
-        case = Frame(top, bg = 'black', relief = 'groove', bd = 0.5, width = 30, height = 30)
-        case.grid(row = i, column = j)
-        grille_graphique[i][j] = case
-
-def mise_a_jour_grille_graph():
-    global piece
-    global grille_graphique
     global grille
+    grille = cree_grille()
+    global grille_graphique
+    grille_graphique = [[0 for _ in range(10)] for _ in range(22)]
 
-    grille_provisoire = copy.deepcopy(grille)
-    forme = piece[2]
-    for c in coordonees(piece):
-        grille_provisoire[c[0]][c[1]] = forme + 1
+    global piece
+    piece = generer_piece()
+
     for i in range(22):
         for j in range(10):
-            grille_graphique[i][j].config(bg = piece_coleur[grille_provisoire[i][j]])
+            case = Frame(top, bg = 'black', relief = 'groove', bd = 0.5, width = 30, height = 30)
+            case.grid(row = i, column = j)
+            grille_graphique[i][j] = case
+
+    def mise_a_jour_grille_graph():
+        global piece
+        global grille_graphique
+        global grille
+
+        grille_provisoire = copy.deepcopy(grille)
+        forme = piece[2]
+        for c in coordonees(piece):
+            grille_provisoire[c[0]][c[1]] = forme + 1
+        for i in range(22):
+            for j in range(10):
+                grille_graphique[i][j].config(bg = piece_coleur[grille_provisoire[i][j]])
 
 
 
-mise_a_jour_grille_graph()
-
-def KeyPressed(event):
-    global piece
-    global grille_graphique
-    d = event.keysym
-    piece = deplacement_piece(grille, piece, d)
     mise_a_jour_grille_graph()
 
+    def KeyPressed(event):
+        global piece
+        global grille_graphique
+        d = event.keysym
+        piece = deplacement_piece(grille, piece, d)
+        mise_a_jour_grille_graph()
 
-top.bind('<Key>', KeyPressed)
-root.mainloop()
-top.mainloop()
+
+
+    top.bind('<Key>', KeyPressed)
+    root.mainloop()
+    top.mainloop()
